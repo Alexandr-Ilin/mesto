55cd04daf47380ac20bc53f7e5a8ecc5be18797e
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-02-05 20:08:25 +0300

    popup добавить

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const elements = document.querySelector('.elements');
 
 function renderCards() {
   initialCards.forEach(creatCard);
-}
+};
 
 function creatCard(item) {
   const newElement = elementsTemplate.cloneNode(true);
@@ -58,44 +58,60 @@ renderCards()
 
 //конец template
 
-const popup = document.querySelector('.popup');
+const popup = document.querySelectorAll('.popup');
+
+const popupEditProfile = document.querySelector('.popup__edit-profile')
+const popupAddCard = document.querySelector('.popup__add-card')
+
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__about-self');
+const addButton = document.querySelector('.profile__add-button')
+
+const closeButtons = document.querySelectorAll('.popup__close');
+
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__about-self');
 const formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__item_type_name');
-let jobInput = formElement.querySelector('.form__item_type_about');
+const nameInput = formElement.querySelector('.form__item_type_name');
+const jobInput = formElement.querySelector('.form__item_type_about');
+
+
 
 //Открыть popup
-function openPopup() {
+function openPopup(el) {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
-  popup.classList.add('popup_opened');
+  //console.log(el)
+  el.classList.add('popup_opened');
 };
 
 //Закрыть popup
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(el) {
+  //event.target.closest('.popup_opened').remove();
+  el.classList.remove('popup_opened');
+
 };
 
+//submit редактирования данных
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupEditProfile);
 }
 
 // Like
-
 // let heartButton = document.querySelector('.element__heart');
-
 // function heartActive() {
 //   heartButton.classList.toggle('element__heart_active');
 // };
-
 // heartButton.addEventListener('click', heartActive);
+
 // Слушатели событий
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', () => openPopup(popupEditProfile));
+addButton.addEventListener('click', () => openPopup(popupAddCard));
+closeButtons.forEach(button => {
+  const popup = button.closest('.popup');
+  console.log(popup);
+  button.addEventListener('click', () => closePopup(popup))
+});
 formElement.addEventListener('submit', formSubmitHandler);
