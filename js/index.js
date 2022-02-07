3780415af7d6846fe051fc7af161ca6ea882dd26
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-02-07 07:31:29 +0300

    на_проверку

--- js/index.js
+++ js/index.js
@@ -23,44 +23,22 @@ const initialCards = [{
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-
-//initialCards.forEach
-
-// function renderElements() {
-//   for (i = 0; i < initialCards.length; i++) {
-//     const newElement = elementsTemplate.cloneNode(true);
-//     newElement.querySelector('.element__place').textContent = initialCards[i].name;
-//     newElement.querySelector('.element__image').setAttribute('src', initialCards[i].link);
-//     elements.append(newElement);
-//   };
-// };
-
-// renderElements()
-
-
-//конец template
-
-//const popup = document.querySelectorAll('.popup');
-
-const popupEditProfile = document.querySelector('.popup_edit_profile')
-const popupAddCard = document.querySelector('.popup_add_card')
-
+//кнопки
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button')
-
 const closeButtons = document.querySelectorAll('.popup__close');
+
 //для редактирования профиля
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__about-self');
-
-const editFormElement = document.querySelector('.form_edit_profile');
+const editFormElement = document.querySelector('.form_type_edit-profile');
 const nameInput = editFormElement.querySelector('.form__item_type_name');
 const jobInput = editFormElement.querySelector('.form__item_type_about');
+
 //для добавления места
 const addFormElement = document.querySelector('.form_edit_place')
 const inputPlace = document.querySelector('.form__item_type_place');
 const inputLink = document.querySelector('.form__item_type_link');
-
 
 //template
 const elementsTemplate = document.querySelector('.elements__template').content;
@@ -68,42 +46,55 @@ const elements = document.querySelector('.elements');
 
 //добавление мест из архива
 function renderCards() {
-  initialCards.forEach(creatCard);
+  initialCards.forEach(appendAddCard);
 };
 
-//добавление мест в низ
+renderCards()
+
+//создание карточки места и обработчиков событий
 function creatCard(item) {
   const newElement = elementsTemplate.cloneNode(true);
   newElement.querySelector('.element__place').textContent = item.name;
   newElement.querySelector('.element__image').src = item.link;
   newElement.querySelector('.element__image').alt = item.name;
   addListeners(newElement);
-  //newElement.querySelector('.element__heart').addEventListener('click', heartActive)
-  elements.append(newElement);
-};
-renderCards()
-
-//добавление мест в начало
-function creatUserCard(item) {
-  const newElement = elementsTemplate.cloneNode(true);
-  newElement.querySelector('.element__place').textContent = item.name;
-  newElement.querySelector('.element__image').src = item.link;
-  newElement.querySelector('.element__image').alt = item.name;
-  addListeners(newElement);
-  elements.prepend(newElement);
+  return newElement;
 };
 
-//Открыть popup
-function openPopup(el) {
+//добавление карточки места вниз списка
+function appendAddCard(item) {
+  elements.append(creatCard(item));
+}
+
+//добавление карт мест в начало и обработчиков
+function prependAddCard(userPlace) {
+  elements.prepend(creatCard(userPlace));
+};
+
+//Открыть popup редактирования профиля
+function openEditProfilePopup() {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
-  //console.log(el)
-  el.classList.add('popup_opened');
+  document.querySelector('.popup_type_edit-profile').classList.add('popup_opened');
 };
+
+//Открыть popup добавления карточки места пользователем
+function openAddCardPopup() {
+  document.querySelector('.form__item_type_place').value = '';
+  document.querySelector('.form__item_type_link').value = '';
+  document.querySelector('.popup_type_add-card').classList.add('popup_opened');
+};
+
+//Открыть popup просмотра фото места
+function openViewPlacePopup(event) {
+  console.log(event.target);
+  document.querySelector('.element-view__image').src = event.target.src;
+  document.querySelector('.element-view__place').textContent = event.target.alt;
+  document.querySelector('.popup_type_view-image').classList.add('popup_opened');
+}
 
 //Закрыть popup
 function closePopup() {
-  //event.target.closest('.popup_opened').remove();
   document.querySelector('.popup_opened').classList.remove('popup_opened');
 };
 
@@ -114,11 +105,9 @@ function addFormSubmitHandler(evt) {
     name: inputPlace.value,
     link: inputLink.value
   };
-  console.log(userPlace);
   closePopup();
-  creatUserCard(userPlace);
+  prependAddCard(userPlace);
 };
-
 
 //submit редактирования данных
 function editFormSubmitHandler(evt) {
@@ -130,30 +119,23 @@ function editFormSubmitHandler(evt) {
 
 //Функция like
 function heartActive(event) {
-  //debugger;
   event.target.classList.toggle('element__heart_active');
 };
 
 //Функция удаление места
 function deletePlace(event) {
-  debugger;
   event.target.closest('.elements__item').remove();
 }
 
-//Функция. добавляющая обработчик
+// Слушатели событий
+//Функция. добавляющая обработчики в создаваемые карточки мест
 function addListeners(el) {
   el.querySelector('.element__heart').addEventListener('click', heartActive);
   el.querySelector('.elements__item-delete').addEventListener('click', deletePlace);
-  el.querySelector('.element__image').addEventListener('click', imageView);
+  el.querySelector('.element__image').addEventListener('click', openViewPlacePopup);
 }
-
-function imageView() {
-  document.querySelector('.popup_view_image').classList.add('popup_opened');
-}
-
-// Слушатели событий
-editButton.addEventListener('click', () => openPopup(popupEditProfile));
-addButton.addEventListener('click', () => openPopup(popupAddCard));
+editButton.addEventListener('click', openEditProfilePopup);
+addButton.addEventListener('click', openAddCardPopup);
 
 closeButtons.forEach((closeButton) => {
   closeButton.addEventListener('click', closePopup);
@@ -161,5 +143,3 @@ closeButtons.forEach((closeButton) => {
 
 editFormElement.addEventListener('submit', editFormSubmitHandler);
 addFormElement.addEventListener('submit', addFormSubmitHandler);
-
-//closeButtons.addEventListener('click', closePopup(event))
