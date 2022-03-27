cb4fd10a790794f69997f3728e062011fb9d9c3c
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-03-27 14:12:55 +0300

    вроде работает

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -3,6 +3,7 @@ export default class UserInfo {
     this._name = document.querySelector(name)
     this._character = document.querySelector(character)
   }
+
   getUserInfo() {
     const userData = {
       name: this._name.textContent,
@@ -10,6 +11,7 @@ export default class UserInfo {
     }
     return userData
   }
+
   setUserInfo = ({ name, link }) => {
     this._name.textContent = name
     this._character.textContent = link
