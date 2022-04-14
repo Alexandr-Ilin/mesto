8d9c6e32d6e5212ca37b55540ee8daa6066fc741
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-04-14 05:49:30 +0300

    22

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,11 +25,11 @@ const validatorEditProfileForm = new FormValidator(validationConfig, '.form_type
 const validatorAddPlaceForm = new FormValidator(validationConfig, '.form_type_edit-place');
 const validatorChengeAvatarForm = new FormValidator(validationConfig, '.form_type_chenge-avatar');
 const popupEditProfile = new PopupWithForm('.popup_type_edit-profile', userInfo.setUserInfo)
-const popupAddPlace = new PopupWithForm('.popup_type_add-card', renderer)
+const popupAddPlace = new PopupWithForm('.popup_type_add-card', handleAddCardFormSubmit)
 const popupChengeAvatar = new PopupWithForm('.popup_type_chenge-avatar', handleChengeAvatarFormSubmit)
 const popupViewPlace = new PopupWithImage('.popup_type_view-image')
 const popupDeletePlace = new PopupDeletePlace('.popup_type_delete-card', deletePlace)
-const section = new Section({ initialCards, renderer }, '.elements')
+const section = new Section(renderNewCard, '.elements')
 const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39',
   headers: {
