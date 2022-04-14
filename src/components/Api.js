a7b3d0f2ac7b10708551886df785ce046e43cc92
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-04-14 04:43:13 +0300

    21

--- src/components/Api.js
+++ src/components/Api.js
@@ -9,108 +9,108 @@ export default class Api {
     this._cardsUrl = `${this._baseUrl}/cards`;
     this._likesUrl = `${this._baseUrl}/cards/likes`;
     this._headers = headers
-    this._handle = (res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    }
+  }
 
-    //получение информации о пользователе с сервера
-    getUserData() {
-      return fetch(this._userUrl, {
-          headers: this._headers
-        })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+  //получение информации о пользователе с сервера
+  getUserData() {
+    return fetch(this._userUrl, {
+        headers: this._headers
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //получение карточек с сервера
-    getInitialCards() {
-      return fetch(this._cardsUrl, {
-          headers: this._headers
-        })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+  //получение карточек с сервера
+  getInitialCards() {
+    return fetch(this._cardsUrl, {
+        headers: this._headers
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //отправка данных пользователя на сервер
-    changeUserData({ user, character }) {
-      return fetch(this._userUrl, {
-          method: 'PATCH',
-          headers: this._headers,
-          body: JSON.stringify({
-            name: user,
-            about: character,
-          })
+  //отправка данных пользователя на сервер
+  changeUserData({ user, character }) {
+    return fetch(this._userUrl, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: user,
+          about: character,
         })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //добавлениt новой карточки на сервер
-    addNewCard({ name, link }) {
-      return fetch(this._cardsUrl, {
-          method: 'POST',
-          headers: this._headers,
-          body: JSON.stringify({
-            name: name,
-            link: link,
-          })
+  //добавлениt новой карточки на сервер
+  addNewCard({ name, link }) {
+    return fetch(this._cardsUrl, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: name,
+          link: link,
         })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //удаление карточки
-    deleteCard(cardId) {
-      return fetch(`${this._cardsUrl}/${cardId}`, {
-          method: 'DELETE',
-          headers: this._headers
-        })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+  //удаление карточки
+  deleteCard(cardId) {
+    return fetch(`${this._cardsUrl}/${cardId}`, {
+        method: 'DELETE',
+        headers: this._headers
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //ставить лайк
-    likedCard(cardId) {
-      return fetch(`${this._likesUrl}/${cardId}`, {
-          method: 'PUT',
-          headers: this._headers
-        })
-        .then(res => {
-          this._handle(res)
-        })
-    }
+  //ставить лайк
+  likedCard(cardId) {
+    return fetch(`${this._likesUrl}/${cardId}`, {
+        method: 'PUT',
+        headers: this._headers
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //удалить лайк
-    deleteLike(cardId) {
-      return fetch(`${this._likesUrl}/${cardId}`, {
-          method: 'DELETE',
-          headers: this._headers
-        })
-        .then(res => {
-          this._handle(res)
+  //удалить лайк
+  deleteLike(cardId) {
+    return fetch(`${this._likesUrl}/${cardId}`, {
+        method: 'DELETE',
+        headers: this._headers
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-        })
-    }
+  //смена аватара
+  chengeAvatar(avatar) {
+    console.log(avatar)
+    return fetch(this._avaUrl, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify(avatar),
+      })
+      .then(res => {
+        this._henle(res)
+      })
+  }
 
-    //смена аватара
-    chengeAvatar(avatar) {
-      console.log(avatar)
-      return fetch(this._avaUrl, {
-          method: 'PATCH',
-          headers: this._headers,
-          body: JSON.stringify(avatar),
-        })
-        .then(res => {
-          this._handle(res)
-        })
+  _henle(res) {
+    if (res.ok) {
+      return res.json();
     }
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
 }
