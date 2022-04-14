293e2b93b675470a8437c82ab936f0634c1b34b9
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-04-14 03:38:07 +0300

    начало

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import UserInfo from "../components/UserInfo.js";
 import { FormValidator } from "../components/FormValidator.js"
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import Api from "../components/Api.js"
+import PopupDeletePlace from "../components/PopupDeletePlace.js";
 
 import {
-  initialCards,
+  buttonAvatar,
   buttonEdit,
   buttonAdd,
   nameInput,
@@ -15,34 +17,45 @@ import {
 } from "../utils/constants.js"
 
 const userInfo = new UserInfo({
-  user: '.profile__name',
-  character: '.profile__about-self'
+  name: '.profile__name',
+  character: '.profile__about-self',
+  avatar: '.profile__avatar'
 })
 const validatorEditProfileForm = new FormValidator(validationConfig, '.form_type_edit-profile');
 const validatorAddPlaceForm = new FormValidator(validationConfig, '.form_type_edit-place');
+const validatorChengeAvatarForm = new FormValidator(validationConfig, '.form_type_chenge-avatar');
 const popupEditProfile = new PopupWithForm('.popup_type_edit-profile', userInfo.setUserInfo)
 const popupAddPlace = new PopupWithForm('.popup_type_add-card', renderer)
+const popupChengeAvatar = new PopupWithForm('.popup_type_chenge-avatar', handleChengeAvatarFormSubmit)
 const popupViewPlace = new PopupWithImage('.popup_type_view-image')
+const popupDeletePlace = new PopupDeletePlace('.popup_type_delete-card', deletePlace)
 const section = new Section({ initialCards, renderer }, '.elements')
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39',
+  headers: {
+    authorization: 'a251447a-ca8d-48d6-88cb-4cedc8f5baae',
+    'Content-Type': 'application/json'
+  }
+});
 
 import './index.css'
 
 validatorEditProfileForm.enableValidation()
 validatorAddPlaceForm.enableValidation()
+validatorChengeAvatarForm.enableValidation()
 
-initialCards.forEach(inicialCardsItem => {
-  renderer(inicialCardsItem)
-})
-
-//инициирует создание
-function renderer(data) {
-  const cardElement = renderNewCard(data) //создает возвращает
-  section.addItem(cardElement); //отрисовывает
-}
-
-//создание новой карточки
-function renderNewCard(data) {
-  const card = new Card(data, '.elements__template', popupViewPlace.open);
+//Новая карточка
+function renderNewCard(data, userId) {
+  const card = new Card(
+    data, {
+      openViewPlacePopup: function(name, link) {
+        popupViewPlace.open(name, link)
+      },
+      openDeleteCardPopup: deletePlace,
+      hanldeLikeButton: () => hanldeLikeButton(card, data)
+    },
+    '.elements__template',
+    userId);
   const cardElement = card.generateCard();
   return cardElement;
 }
@@ -51,7 +64,6 @@ function renderNewCard(data) {
 function openAddCardPopup() {
   validatorAddPlaceForm.validationOpenPopup()
   popupAddPlace.open();
-
 };
 
 //Открыть popup редактирования профиля
@@ -63,9 +75,116 @@ function openpopupEditProfile() {
   popupEditProfile.open();
 };
 
+//Открыть popup смены аватара
+function openChengeAvatarPopup() {
+  validatorChengeAvatarForm.validationOpenPopup()
+  popupChengeAvatar.open()
+}
+
+//Замена аватара
+function handleChengeAvatarFormSubmit(avatar) {
+  popupChengeAvatar.hendleLoad(true)
+  api.chengeAvatar(avatar)
+    .then((res) => {
+      console.log(res)
+      userInfo.setUserInfo(res)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      popupChengeAvatar.hendleLoad(false);
+    })
+}
+
+//Функция удаления карточки
+function deletePlace(evt, card) {
+  if (evt.type === 'submit') {
+    evt.preventDefault()
+    api.deleteCard(card._cardId)
+      .then(() => {
+        card.deletePlace()
+        popupDeletePlace.close()
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  popupDeletePlace.open(card)
+}
+
+//Обработка лайков
+function hanldeLikeButton(card) {
+  if (!card.pointLike()) {
+    api.likedCard(card._cardId)
+      .then((res) => {
+        card.changeLike(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    api.deleteLike(card._cardId)
+      .then((res) => {
+        card.changeLike(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+//Редактирование профиля
+function handleProfileFormSubmit(data) {
+  popupEditProfile.hendleLoad(true)
+  api.changeUserData(data)
+    .then((data) => {
+      userInfo.setUserInfo(data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      popupEditProfile.hendleLoad(false);
+    })
+}
+
+//Добавление карточки
+function handleAddCardFormSubmit(data) {
+  popupAddPlace.hendleLoad(true)
+  api.addNewCard(data)
+    .then((res) => {
+      section.addItemPrepend(res, res.owner._id)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      popupAddPlace.hendleLoad(false);
+    })
+}
+
+
+//Слушатели
 buttonEdit.addEventListener('click', openpopupEditProfile);
 buttonAdd.addEventListener('click', openAddCardPopup);
+buttonAvatar.addEventListener('click', openChengeAvatarPopup);
 
+popupDeletePlace.setEventListeners()
+popupChengeAvatar.setEventListeners()
 popupEditProfile.setEventListeners()
 popupAddPlace.setEventListeners()
 popupViewPlace.setEventListeners()
+
+//Начальные карточки
+Promise.all([
+    api.getUserData(),
+    api.getInitialCards()
+  ])
+  .then((arr) => {
+    userInfo.setUserInfo(arr[0])
+    section.renderItems(arr[1], arr[0]._id);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,32 +1,7 @@
-export const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-];
-
 //кнопки
 export const buttonEdit = document.querySelector('.profile__edit-button');
-export const buttonAdd = document.querySelector('.profile__add-button')
+export const buttonAdd = document.querySelector('.profile__add-button');
+export const buttonAvatar = document.querySelector('.profile__avatar-button');
 
 //для редактирования профиля
 export const nameInput = document.querySelector('.form__item_type_name');
